acf0aec43b636d4814309a118a1d04c4b0396f23
ghiroma <guillermo.hiroma@gmail.com>
2013-11-03 11:51:28 -0300

    Esta subido la base de datos en memoria

--- src/dataTier/DataAccess.java
+++ src/dataTier/DataAccess.java
@@ -15,8 +15,22 @@ public final class DataAccess{
 	private DataAccess(){
 		try{
 		Class.forName("org.h2.Driver");
-		conn = DriverManager.getConnection("jdbc:h2:~/test","sa","");
+		//Para testeo se hara una base de datos en memoria.
+		//TODO cambiar a servidor embebido para produccion.
+//		conn = DriverManager.getConnection("jdbc:h2:~/test","sa","");
+		conn = DriverManager.getConnection("jdbc:h2:mem:");
 		stat = conn.createStatement();
+		
+		//Solo para base de datos en memoria.
+		stat.execute("CREATE TABLE Usuarios(User varchar(100) primary key, Password varchar(100), Mail varchar(100), Apyn varchar(100), FechaNacimiento DateTime, FechaAlta DateTime, Conectado bit)");
+		stat.execute("CREATE TABLE Amigos(User1 varchar(100), User2 varchar(100),Primary Key(User1,User2) ,FOREIGN KEY(User1) references Usuarios(User), Foreign Key(User2) references Usuarios(User))");
+		stat.execute("CREATE TABLE LogLogin(User varchar(100), FechaHoraInicio DateTime,Primary Key(User,FechaHoraInicio),FOREIGN KEY(User) references Usuarios(User))");
+		stat.execute("CREATE TABLE LogPenalizacion(ID int primary key auto_increment, User varchar(100), Descripcion varchar(250), FechaFin DateTime, Foreign key(User) references Usuarios(User))");
+		stat.execute("INSERT INTO Usuarios (User,Password,Mail,FechaAlta,Apyn,FechaNacimiento,Conectado)VALUES('Usuario1','Contrsenia','Mail', '2011-01-01 00:30:00','Perez','1991-01-01',0),('Usuario2','Contrsenia','Mail', '2011-01-01 00:22:00','Lopez', '1991-01-01',1),('Usuario3','Contrsenia','Mail', '2011-01-01 01:30:00','Susana', '1991-01-01',1),('Usuario4','Contrsenia','Mail', '2011-01-01 03:20:00','Mirta', '1991-01-01',1),('Usuario5','Contrsenia','Mail' ,'2011-01-01 04:40:00', 'Cintia','1991-01-01',1),('Usuario6','Contrsenia','Mail', '2011-01-01 05:30:00', 'Jose','1991-01-01',0)");
+		stat.execute("INSERT INTO logLogin(User,FechaHoraInicio) VALUES('Usuario1','2013-10-13 00:30:00'),('Usuario2','2013-10-13 00:22:00'),('Usuario3', '2013-10-13 01:30:00'),('Usuario4', '2013-10-13 03:20:00'),('Usuario5','2013-10-20 04:40:00'),('Usuario6', '2013-10-15 05:30:00')");
+		stat.execute("INSERT INTO Amigos (User1,User2) VALUES ('Usuario1','Usuario3'),('Usuario1','Usuario4'),('Usuario1','Usuario5'),('Usuario1','Usuario6')");
+		stat.execute("INSERT INTO LogPenalizacion (User,Descripcion,FechaFin) VALUES('Usuario3','Ofensa','2013-10-30')");
+		//
 		}
 		catch(Exception ex)
 		{
@@ -142,8 +156,8 @@ public final class DataAccess{
 			ArrayList<FriendStatus> friendList = new ArrayList<FriendStatus>();
 			ResultSet rSet;
 			
-			String statement = "SELECT U1.User,Conectado FROM USUARIOS U1 INNER JOIN (SELECT Usuario1 as User FROM AMIGOS WHERE Usuario2='" +user.getUser()+"'"
-					+ "UNION SELECT Usuario2 as User FROM AMIGOS WHERE Usuario1='" +user.getUser() +"') AS U2 ON U1.User= U2.User "; 
+			String statement = "SELECT U1.User,Conectado FROM USUARIOS U1 INNER JOIN (SELECT User1 as User FROM AMIGOS WHERE User2='" +user.getUser()+"'"
+					+ "UNION SELECT User2 as User FROM AMIGOS WHERE User1='" +user.getUser() +"') AS U2 ON U1.User= U2.User "; 
 			rSet = stat.executeQuery(statement);
 			
 			while(rSet.next())
@@ -187,8 +201,7 @@ public final class DataAccess{
 	public BanInfo checkBan(UserMetaData user){
 		//Devuelve cantidad de dias restantes y descripcion.
 		try{
-			//select p.descripcion, DateDiff('day',CURRENT_DATE(),p.fechafin) from log_penalizacion p
-			 String statement = "SELECT p.Descripcion, DateDiff('day',CURRENT_DATE(),p.fechafin) as Restantes from LOG_PENALIZACION p INNER JOIN USUARIOS u on u.User = p.user WHERE p.User='"+ user.getUser()+"'";
+			 String statement = "SELECT p.Descripcion, DateDiff('day',CURRENT_DATE(),p.fechafin) as Restantes from LOGPENALIZACION p INNER JOIN USUARIOS u on u.User = p.user WHERE p.User='"+ user.getUser()+"'";
 			 ResultSet rs = stat.executeQuery(statement);
 			 if(!rs.first())
 				 return null;
@@ -205,7 +218,7 @@ public final class DataAccess{
 	public static void main(String arg[])
 	{
 		UserMetaData data = new UserMetaData();
-		data.setUser("Usuario1");
+		data.setUser("Usuario3");
 		data.setPassword("Contrasenia");
 		BanInfo info = DataAccess.getInstance().checkBan(data);
 	}
