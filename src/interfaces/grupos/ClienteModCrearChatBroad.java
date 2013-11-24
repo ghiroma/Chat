e9b1756f2d9a191d86a97a112113db1915ddb9c6
sebajuffar <seba.juffar@gmail.com>
2013-11-24 16:12:48 -0300

    Grupos - Funcionalidad de la interfaz Crear Sala

--- src/interfaces/grupos/ClienteModCrearChatBroad.java
+++ src/interfaces/grupos/ClienteModCrearChatBroad.java
@@ -6,21 +6,25 @@ import java.awt.Font;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JEditorPane;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.ListModel;
 import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
 
 import client.ChatClient;
 
+import common.FriendStatus;
 import common.MensajeGrupo;
 
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.util.ArrayList;
 import java.util.List;
 import javax.swing.JList;
 import javax.swing.JTable;
@@ -51,13 +55,101 @@ public class ClienteModCrearChatBroad extends JFrame {
 		lblCrearGrupoDe.setBounds(10, 11, 461, 47);
 		contentPane.add(lblCrearGrupoDe);
 		
-		JButton btnNewButton = new JButton("->");
-		btnNewButton.setBounds(211, 186, 48, 33);
-		contentPane.add(btnNewButton);
+		final JList listAmigos = new JList();
+		listAmigos.setBounds(20, 149, 181, 245);
+		contentPane.add(listAmigos);
+		listAmigos.setModel(this.obtenerListaAmigos());
 		
-		JButton button = new JButton("<-");
-		button.setBounds(211, 230, 48, 33);
-		contentPane.add(button);
+		
+		final JList listAmigosAgregados = new JList();
+		listAmigosAgregados.setBounds(269, 149, 191, 245);
+		contentPane.add(listAmigosAgregados);
+		
+		JButton btnAgregarAmigo = new JButton("->");
+		btnAgregarAmigo.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				// Obtengo el usuario que se seleccionó
+				String nombreUsuario = listAmigos.getSelectedValue().toString();
+				// Hago el cambio entre la lista de amigos y la lista de amigos agregados
+				// Lo saco de la lista de amigos y lo pongo en la lista de amigos agregados
+				listAmigosAgregados.setModel(this.agregarAmigo(nombreUsuario));
+				listAmigos.setModel(this.removerAmigo(nombreUsuario));
+			}
+			private DefaultListModel<String> agregarAmigo(String nombreUsuario){
+				DefaultListModel<String> modelAmigos = new DefaultListModel<String>();
+				boolean existe = false;
+				// Recorro toda la lista de amigos agregados para que quede cargada con todos los elemtos que tenía
+				for(int i = 0; i < listAmigosAgregados.getModel().getSize(); i++) {
+					Object item = listAmigosAgregados.getModel().getElementAt(i);
+					modelAmigos.addElement(item.toString());
+					// Valido que el usuario no esté ya agregado
+					if(item.toString() == nombreUsuario){
+						existe = true;
+					}
+				}
+				if(!existe){
+					modelAmigos.addElement(nombreUsuario);	
+				}
+				return modelAmigos;
+			}
+			private DefaultListModel<String> removerAmigo(String nombreUsuario){
+				DefaultListModel<String> modelAmigos = new DefaultListModel<String>();
+				// Vuelvo a cargar la lista de amigos con todos los amigos menos el que se agregó
+				for(int i = 0; i < listAmigos.getModel().getSize(); i++) {
+					Object item = listAmigos.getModel().getElementAt(i);
+					if(item.toString() != nombreUsuario){
+						modelAmigos.addElement(item.toString());
+					}
+				}
+				return modelAmigos;
+			}
+		});
+		btnAgregarAmigo.setBounds(211, 186, 48, 33);
+		contentPane.add(btnAgregarAmigo);
+		
+		JButton btnRemoverAmigo = new JButton("<-");
+		btnRemoverAmigo.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				// Obtengo el usuario que se seleccionó
+				String nombreUsuario = listAmigosAgregados.getSelectedValue().toString();
+				// Hago el cambio entre la lista de amigos y la lista de amigos agregados
+				// Lo saco de la lista de amigos y lo pongo en la lista de amigos agregados
+				listAmigos.setModel(this.agregarAmigo(nombreUsuario));
+				listAmigosAgregados.setModel(this.removerAmigo(nombreUsuario));
+			}
+			private DefaultListModel<String> agregarAmigo(String nombreUsuario){
+				DefaultListModel<String> modelAmigos = new DefaultListModel<String>();
+				boolean existe = false;
+				// Recorro toda la lista de amigos agregados para que quede cargada con todos los elemtos que tenía
+				for(int i = 0; i < listAmigos.getModel().getSize(); i++) {
+					Object item = listAmigos.getModel().getElementAt(i);
+					modelAmigos.addElement(item.toString());
+					// Valido que el usuario no esté ya agregado
+					if(item.toString() == nombreUsuario){
+						existe = true;
+					}
+				}
+				if(!existe){
+					modelAmigos.addElement(nombreUsuario);	
+				}
+				return modelAmigos;
+			}
+			private DefaultListModel<String> removerAmigo(String nombreUsuario){
+				DefaultListModel<String> modelAmigos = new DefaultListModel<String>();
+				// Vuelvo a cargar la lista de amigos con todos los amigos menos el que se agregó
+				for(int i = 0; i < listAmigosAgregados.getModel().getSize(); i++) {
+					Object item = listAmigosAgregados.getModel().getElementAt(i);
+					if(item.toString() != nombreUsuario){
+						modelAmigos.addElement(item.toString());
+					}
+				}
+				return modelAmigos;
+			}
+		});
+		btnRemoverAmigo.setBounds(211, 230, 48, 33);
+		contentPane.add(btnRemoverAmigo);
 		
 		JLabel lblNombreDelGrupo = new JLabel("NOMBRE DEL GRUPO: ");
 		lblNombreDelGrupo.setBounds(10, 69, 191, 14);
@@ -80,27 +172,28 @@ public class ClienteModCrearChatBroad extends JFrame {
 				
 			}
 		});
-		JList listAmigos = new JList();
-		listAmigos.setBounds(20, 149, 181, 245);
-		contentPane.add(listAmigos);
-		
-		JList listAmigosAgregados = new JList();
-		listAmigosAgregados.setBounds(269, 149, 191, 245);
-		contentPane.add(listAmigosAgregados);
 		
 		btnCrearSala.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
 				String nombre = txtNombreGrupo.getText();
 				String emisor = ChatClient.getInstance().getUsuarioLogeado().getUser();
-				List<String> usuarios = null;
-				//TODO: hacer que tome el listado de amigos que estan en listAmigosAgregados y guardarlos en usuarios
+				List<String> usuarios = new ArrayList<String>();
+				// Recorro toda la lista de los usuarios que se agregaron y los agrego a usuarios
+				for(int i = 0; i < listAmigosAgregados.getModel().getSize(); i++) {
+					Object item = listAmigosAgregados.getModel().getElementAt(i);
+					usuarios.add(item.toString());
+				}
+				// Creo el grupo
 				Grupo grupo = new Grupo(nombre, usuarios);
+				// Creo el mensaje que se envia a todos los usuarios del grupo
 				MensajeGrupo mensaje = new MensajeGrupo(grupo, emisor, "");
-				//if(!usuarios.isEmpty() && !nombre.isEmpty()){
+				// Valido que esté todo bien antes de mostrar la pantalla
+				// Se valida que haya usuarios agregados y que tenga nombre el grupo
+				if(!usuarios.isEmpty() && !nombre.isEmpty()){
 					ClienteModSalaDeChat salaDeChat = new ClienteModSalaDeChat();
 					salaDeChat.setVisible(true);	
-				//}
+				}
 			}
 		});
 		btnCrearSala.setBounds(251, 419, 107, 23);
@@ -115,9 +208,15 @@ public class ClienteModCrearChatBroad extends JFrame {
 		});
 		btnCancelar.setBounds(368, 419, 89, 23);
 		contentPane.add(btnCancelar);
-		
-		
-		
-		
 	}
+	
+	private DefaultListModel<String> obtenerListaAmigos() {
+		DefaultListModel<String> modelAmigos = new DefaultListModel<String>();
+		List<FriendStatus> amigos = ChatClient.getInstance().getAmigos();
+		for (FriendStatus amigo : amigos) {
+			if(amigo.getEstado() == 1)
+				modelAmigos.addElement(amigo.getUsername());
+		}
+		return modelAmigos;
+	}	
 }
