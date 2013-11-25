572ae9b693207fcefc161087d3831ece171b03c3
ghiroma <guillermo.hiroma@gmail.com>
2013-11-25 19:55:36 -0300

    conflictos

--- src/common/Mensaje.java
+++ src/common/Mensaje.java
@@ -40,19 +40,11 @@ public class Mensaje implements Serializable {
 	public static final int RESPUESTA_PIZARRA = 27;
 	public static final int ACTUALIZACION_PIZARRA = 28;
 	public static final int RESPUESTA_ACTUALIZACION_PIZARRA = 29;
-<<<<<<< HEAD
 	public static final int ENVIAR_MENSAJE_TATETI = 30;
 	
 	/* Grupos */
 	public static final int CREAR_GRUPO = 100; 
-=======
 	// Fin: TATETI
-
-	// Inicio: GRUPOS
-	public static final int CREAR_GRUPO = 100;
-	// Fin: GRUPOS
-
->>>>>>> branch 'master' of https://github.com/ghiroma/Chat.git
 
 	private int id;
 	private Object cuerpo;
